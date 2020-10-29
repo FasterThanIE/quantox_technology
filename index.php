9dfb8c8dc5ff81eba145492722bbe49fbb1b9c3e
FasterThanIE <tomicawarr@gmail.com>
2020-10-29 19:56:42 +0100

    * Fake router

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once "vendor/autoload.php";
 
 if(isset($_GET['student']) && $_GET['student'] >= 1)
 {
-    $student = filter_var($_GET['student'],[FILTER_VALIDATE_INT]);
+    $student = filter_var($_GET['student'],FILTER_VALIDATE_INT);
     \App\Controllers\HomeController::handleGrade($student);
 }
